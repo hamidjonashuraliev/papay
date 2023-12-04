e593fc4a3c4a9c20ad8a1b27e2ac60296fefd982
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2023-12-04 21:26:47 +0900

    feat: develop Product controller logic

---
+++ controllers/productController.js
@@ -0,0 +1,28 @@
+let productController = module.exports;
+
+productController.getAllProducts = async (req, res) => {
+    try {
+        console.log("GET: controller/getAllProducts");
+    } catch {
+        console.log(`Error, controller/getAllProducts, ${err.message}`);
+        res.json({ state: "fail", message: err.message });
+    }
+};
+
+productController.addNewProduct = async (req, res) => {
+    try {
+        console.log("POST: controller/addNewProduct");
+        console.log(req.member);
+        // TODO: product creation develop
+    } catch {
+        console.log(`Error, controller/addNewProduct, ${err.message}`);
+    }
+};
+
+productController.updateChoosenProduct = async (req, res) => {
+    try {
+        console.log("POST: controller/updateChoosenProduct");
+    } catch {
+        console.log(`Error, controller/updateChoosenProduct, ${err.message}`);
+    }
+};
--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -70,6 +70,18 @@ restaurantController.logout = (req, res) => {
     res.send("You are in logout section");
 };
 
+restaurantController.validateAuthRestaurant = (req, res, next) => {
+    if (req.session?.member?.mb_type === "RESTAURANT") {
+        req.member = req.session.member;
+        next();
+    } else
+        res.json({
+            state: "fail",
+            message:
+                "Only authenticated members with restaurant type are allowed",
+        });
+};
+
 restaurantController.checkSessions = (req, res) => {
     if (req.session?.member) {
         res.json({ state: "succeed", data: req.session.member });
