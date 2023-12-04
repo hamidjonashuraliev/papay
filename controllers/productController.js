3591d71e2d76ded0128323a709fed930c444f80a
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2023-12-05 08:30:28 +0900

    feat: yangi productlarni restaurant userlar hosil qilish

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,8 @@
 // product-related controller in papay adminka web application
+const assert = require("assert"); // A Node.js built-in module for making assertions.
+const Definer = require("../lib/mistake"); // A utility class that contains predefined error messages.
 let productController = module.exports;
+const Product = require("../models/Product"); //  imports the Product model, which a class responsible for handling database interactions related to products.
 
 // This is an asynchronous function designed to handle an HTTP GET request to retrieve all products.
 productController.getAllProducts = async (req, res) => {
@@ -15,12 +18,25 @@ productController.getAllProducts = async (req, res) => {
 productController.addNewProduct = async (req, res) => {
     try {
         console.log("POST: controller/addNewProduct");
+        // console.log("Request files: ", req.files);
+        assert(req.files, Definer.general_err3);
         // console.log(req.member);
         // res.json({ state: "success", test: "OK" });
-        res.send("OK");
+        const product = new Product();
+        let data = req.body;
 
-        // TODO: product creation develop
-    } catch {
+        data.product_images = req.files.map((ele) => {
+            return ele.path;
+        });
+
+        const result = await product.addNewProductData(data, req.member);
+
+        const html = `<script>
+                    alert(new dish added successfully);
+                    window.location.replace('/resto/products/menu');
+                  </script>`;
+        res.end(html);
+    } catch (err) {
         console.log(`Error, controller/addNewProduct, ${err.message}`);
     }
 };
@@ -29,7 +45,7 @@ productController.addNewProduct = async (req, res) => {
 productController.updateChoosenProduct = async (req, res) => {
     try {
         console.log("POST: controller/updateChoosenProduct");
-    } catch {
+    } catch (err) {
         console.log(`Error, controller/updateChoosenProduct, ${err.message}`);
     }
 };
--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,27 @@
+const assert = require("assert"); // A module for asserting truthy values.
+const { shapeIntoMongooseObjectId } = require("../lib/config");
+// utility function that converts some value into a Mongoose ObjectID.
+const Definer = require("../lib/mistake");
 const ProductModel = require("../schema/product.model");
+
+class Product {
+    constructor() {
+        this.productModel = ProductModel;
+    }
+
+    async addNewProductData(data, member) {
+        try {
+            data.restaurant_mb_id = shapeIntoMongooseObjectId(member._id);
+
+            const new_product = new this.productModel(data);
+            const result = await new_product.save();
+
+            assert.ok(result, Definer.product_err1);
+            return result;
+        } catch (err) {
+            throw err;
+        }
+    }
+}
+
+module.exports = Product;
