e0ed77cfa31ba0b9cff738fe16901417c2fd4768
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2023-12-12 05:15:22 +0900

    feat:  fulfil development  order & orderitems api business logic

--- models/Order.js
+++ models/Order.js
@@ -6,58 +6,94 @@ const OrderItemModel = require("../schema/order_item.model");
 // The Order class in this code is designed to manage order data in a Node.js application, interfacing with MongoDB through Mongoose models.
 
 class Order {
-  constructor() {
-    this.orderModel = OrderModel;
-    this.orderItemModel = OrderItemModel;
-  }
-
-  async createOrderData(member, data) {
-    //  This method calculates the total amount of an order by iterating over each item in the data array, summing up the products of quantities and prices. It adds a delivery cost if the total amount is below a certain threshold, then saves the order data using saveOrderData method and logs the generated order ID.
-    try {
-      let order_total_amount = 0,
-        delivery_cost = 0;
-      const mb_id = shapeIntoMongooseObjectId(member._id);
-
-      data.map((item) => {
-        order_total_amount += item["quantity"] * item["price"];
-      });
-
-      if (order_total_amount < 100) {
-        delivery_cost = 4;
-        order_total_amount += delivery_cost;
-      }
-
-      const order_id = await this.saveOrderData(
-        order_total_amount,
-        delivery_cost,
-        mb_id
-      );
-
-      console.log("order_id::", order_id);
-      return order_id;
-    } catch (err) {
-      throw err;
+    constructor() {
+        this.orderModel = OrderModel;
+        this.orderItemModel = OrderItemModel;
     }
-  }
 
-  async saveOrderData(order_total_amount, delivery_cost, mb_id) {
-    // This method creates a new order document with the total amount, delivery cost, and member ID (mb_id). It saves the order to the database using the orderModel and returns the _id of the created order. It includes error handling with assertions and custom error messages.
-    try {
-      const new_order = new this.orderModel({
-        order_total_amount: order_total_amount,
-        order_delivery_cost: delivery_cost,
-        mb_id: mb_id,
-      });
+    async createOrderData(member, data) {
+        //  This method calculates the total amount of an order by iterating over each item in the data array, summing up the products of quantities and prices. It adds a delivery cost if the total amount is below a certain threshold, then saves the order data using saveOrderData method and logs the generated order ID.
+        try {
+            let order_total_amount = 0,
+                delivery_cost = 0;
+            const mb_id = shapeIntoMongooseObjectId(member._id);
 
-      const result = await new_order.save();
-      assert.ok(result, Definer.order_err1);
+            data.map((item) => {
+                order_total_amount += item["quantity"] * item["price"];
+            });
 
-      return result._id;
-    } catch (err) {
-      console.log(err);
-      throw new Error(Definer.order_err1);
+            if (order_total_amount < 100) {
+                delivery_cost = 4;
+                order_total_amount += delivery_cost;
+            }
+
+            const order_id = await this.saveOrderData(
+                order_total_amount,
+                delivery_cost,
+                mb_id
+            );
+
+            console.log("order_id::", order_id);
+
+            await this.recordOrderItemsData(order_id, data);
+
+            return order_id;
+        } catch (err) {
+            throw err;
+        }
     }
-  }
+
+    async saveOrderData(order_total_amount, delivery_cost, mb_id) {
+        // This method creates a new order document with the total amount, delivery cost, and member ID (mb_id). It saves the order to the database using the orderModel and returns the _id of the created order. It includes error handling with assertions and custom error messages.
+        try {
+            const new_order = new this.orderModel({
+                order_total_amount: order_total_amount,
+                order_delivery_cost: delivery_cost,
+                mb_id: mb_id,
+            });
+
+            const result = await new_order.save();
+            assert.ok(result, Definer.order_err1);
+
+            return result._id;
+        } catch (err) {
+            console.log(err);
+            throw new Error(Definer.order_err1);
+        }
+    }
+    async recordOrderItemsData(order_id, data) {
+        try {
+            const pro_list = data.map(async (item) => {
+                return await this.saveOrderItemsData(item, order_id);
+            });
+            const results = await Promise.all(pro_list);
+            console.log("results:::", results);
+            return true;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async saveOrderItemsData(item, order_id) {
+        try {
+            order_id = shapeIntoMongooseObjectId(order_id);
+            item._id = shapeIntoMongooseObjectId(item._id);
+
+            const order_item = new this.orderItemModel({
+                item_quantity: item["quantity"],
+                item_price: item["price"],
+                order_id: order_id,
+                product_id: item["_id"],
+            });
+
+            const result = await order_item.save();
+            assert.ok(result, Definer.order_err2);
+            return "inserted";
+        } catch (err) {
+            console.log(err);
+            throw new Error(Definer.order_err2);
+        }
+    }
 }
 
 module.exports = Order;
